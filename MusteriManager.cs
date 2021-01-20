9bd46da5839079ced0fae169a258c83e5b793df6
burak971 <53896580+burak971@users.noreply.github.com>
2021-01-20 15:18:19 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Customer was added : " + musteri.Id);
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Customer was deleted." + musteri.Id);
+        }
+        public void Liste(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listesi: " + musteri.Id );
+          
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = ("Burak");
+            musteri1.Soyad = ("Karapınar");
+            musteri1.Id= 123456789;
+            
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = ("Mesut");
+            musteri2.Soyad = ("özil");
+            musteri2.Id= 987654324;
+            
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = ("Polat");
+            musteri3.Soyad = ("Alemdar");
+            musteri3.Id= 741258963;
+            
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            Console.WriteLine("PROFİLİNİZ OLUŞTURULUYOR ");
+            Console.WriteLine(" ");
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+            Console.WriteLine(" ");
+            Console.WriteLine("MÜŞTERİLER LİSTELENİYOR");
+            Console.WriteLine(" ");
+            musteriManager.Liste(musteri1);
+            Console.WriteLine(" ");
+            musteriManager.Liste(musteri2);
+            Console.WriteLine(" ");
+            musteriManager.Liste(musteri3);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("MÜŞTERİLER SİLİNİYOR");
+            Console.WriteLine(" ");
+            musteriManager.Delete(musteri1);
+            musteriManager.Delete(musteri2);
+        }
+    }
+}
